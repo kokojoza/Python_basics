bf33b7229ffcf1558635b7b7f4af38e8a34c3182
kos.pavlov@gmail.com <InOOEGxzOEXwdHMQmjN>
2020-09-26 12:28:57 +0700

    работа над ошибками2

--- homework/les4/task1.py
+++ homework/les4/task1.py
@@ -8,11 +8,13 @@ def payroll(output_in_hours: float, rate_per_hour: float, premium:float) -> floa
 
 from sys import argv
 
-script_name, output_in_hours, rate_per_hour, premium, *_ = argv
+if __name__ == '__main__':
 
-try:
-    result = payroll(float(output_in_hours), float(rate_per_hour), float(premium))
-    print(f"'ЗП: '{result}")
-except ValueError as e:
-    print('Ошибка ввода')
-    print(e)
+    script_name, output_in_hours, rate_per_hour, premium, *_ = argv
+
+    try:
+        result = payroll(float(output_in_hours), float(rate_per_hour), float(premium))
+        print(f"'ЗП: '{result}")
+    except ValueError as e:
+        print('Ошибка ввода')
+        print(e)
